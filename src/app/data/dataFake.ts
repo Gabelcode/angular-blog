daa5cd8ecbebc38c0a8d821ee72cb39f48b227a0
Gabriel Lima <gabelcodedev@gmail.com>
2023-04-12 09:46:21 -0300

    Projeto finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "10 linguagens de programação que empresas procuram em candidatos",
+    "description": "A verdade é que ter conhecimento sobre qualquer linguagem de programação não é o suficiente para conseguir uma boa vaga o interessado nesse setor deve ter noção do que o mercado procura, e se aprimorar em seu uso.",
+    "photoCover":"https://t.ctcdn.com.br/Esa5K4KnYGSl1P7Pa1bUz_fQA8o=/720x405/smart/filters:format(webp)/i252888.jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Educação gratuita em TI",
+    "description": "Dio vem forte",
+    "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2021/10/educac%CC%A7a%CC%83o-inovac%CC%A7a%CC%83o.jpeg"
+  },
+  {
+    "id":3,
+    "title": "A importância e os desafios do inglês na vida de um Desenvolvedor",
+    "description": "lalalala",
+    "photoCover":"https://hermes.digitalinnovation.one/articles/cover/91bcaf62-646f-438c-bd54-4b2178fa23b9.jpg"
+  },
+  {
+    "id":4,
+    "title": "Dia do estagiário",
+    "description": "lalalala",
+    "photoCover":"https://www.centralestagio.com/site2023/wp-content/uploads/2021/08/Dia-do-Estagiario.png"
+  }
+]
